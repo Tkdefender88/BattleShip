23911adc20e529a206b6bcf93d638593f234b5e6
juice <jbak@mtech.edu>
2020-05-14 11:41:03 -0600

    started adding first component

--- dart/lib/app_component.dart
+++ dart/lib/app_component.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
 
-import 'src/todo_list/todo_list_component.dart';
+import 'src/header/bs_header_component.dart';
 
 // AngularDart info: https://angulardart.dev
 // Components info: https://angulardart.dev/components
@@ -9,7 +9,7 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [BsHeaderComponent],
 )
 class AppComponent {
   // Nothing here yet. All logic is in TodoListComponent.
