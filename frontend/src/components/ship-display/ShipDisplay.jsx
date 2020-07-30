ee33f38beab3b6f573cc029fc4171c8c73c861bf
Justin <tkdefender88@gmail.com>
2020-07-30 17:38:34 -0600

    ocean grid

--- frontend/src/components/ship-display/ShipDisplay.jsx
+++ frontend/src/components/ship-display/ShipDisplay.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import style from './ship-display.module.css';
+import ship from '../../images/carrier.png';
 
 class ShipDisplay extends Component {
     render() {
         return (
             <div className={style.ship_display}>
                 <div>
-                    <img src="images/battleship.png"></img>
+                    <img src={ship}></img>
                 </div>
                 <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
                     <ToggleButton value={1}>Horizontal</ToggleButton>
